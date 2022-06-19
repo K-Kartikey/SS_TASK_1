9e4b79e96b25a5b886bf1e6e04c2f58028b2b0a4
Kumar Kartikey <kartikey.k@safe.security>
2022-06-19 23:00:09 +0530

    SQL Configured.

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var mysql2 = require("mysql2");
+var bodyparser = require("body-parser");
 var app = express();
+app.use(bodyparser.json());
+var mysqlconnection = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "kamalkamal2910",
+    database: "SS_Books"
+});
+mysqlconnection.connect(function (err) {
+    if (!err) {
+        console.log("Connected Sucessfully");
+    }
+    else {
+        console.log("Connection Failed");
+    }
+});
 app.get('/', function (req, res) {
-    res.json({ Name: 'KK',
-        Contact: 9711566660,
-        Email_Id: 'kk@gmail.com'
+    res.json({ Staus: "Connected to BOOKS API."
     });
 });
+app.get('/books', function (req, res) {
+    mysqlconnection.query("SELECT * FROM BOOK", function (err, rows, fields) {
+        if (!err) {
+            res.json(rows);
+        }
+        else {
+            console.log(err);
+        }
+    });
+});
+// app.post('/create',(req,res)=>{
+//   req.body()
+// });
 app.listen(3000, function () {
     console.log('The application is listening on port 3000!');
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,50 @@
 import * as express from 'express';
+import * as mysql2 from 'mysql2';
+import * as bodyparser from 'body-parser';
+
 const app = express();
+app.use(bodyparser.json());
+
+const mysqlconnection =mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "kamalkamal2910",
+  database: "SS_Books"
+})
+
+mysqlconnection.connect((err)=>{
+  if(!err)
+  {
+    console.log("Connected Sucessfully");
+  }
+  else
+  {
+    console.log("Connection Failed");
+  }
+})
 
 app.get('/', (req, res) => {
-  res.json({ Name : 'KK',
-  Contact : 9711566660,
-  Email_Id : 'kk@gmail.com'
+  res.json({ Staus : "Connected to BOOKS API."
 });
 });
+
+app.get('/books',(req,res)=>{
+  mysqlconnection.query("SELECT * FROM BOOK",(err,rows,fields)=>{
+    if(!err)
+    {
+      res.json(rows);
+    }
+    else{
+      console.log(err);
+    }
+  })
+})
+
+// app.post('/create',(req,res)=>{
+//   req.body()
+// });
+
+
 
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
